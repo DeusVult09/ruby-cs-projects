75379a2239c1fec6b1fb91410bbc96bcc9746d3f
Zarifa <mamadalievazara@gmail.com>
2025-12-02 01:08:13 +0100

    Write: logic -> has? method, include .any? as a second option

--- hashmap/lib/hashmap.rb
+++ hashmap/lib/hashmap.rb
@@ -1,10 +1,12 @@
 class HashMap
   LOAD_FACTOR = 0.75
   CAPACITY = 16
+  THRESHOLD = (CAPACITY * LOAD_FACTOR).floor
 
   def initialize
     @load_factor = LOAD_FACTOR
     @capacity = CAPACITY
+    @threshold = THRESHOLD
     @size = 0
     @buckets = Array.new(@capacity) { [] }
   end
@@ -21,6 +23,8 @@ class HashMap
   end
 
   def set(key, value)
+    resize if needs_resize?
+
     index = bucket_index(key)
     bucket = @buckets[index]
 
@@ -29,17 +33,58 @@ class HashMap
         pair[1] = value
         return
       end
-    end
-
-    if @size / @capacity > @load_factor
-      resize
-    end
-    
+    end 
     bucket << [key, value]
     @size += 1
   end
 
+  def needs_resize?
+    (@size + 1) >  @threshold
+  end
+
   def resize
-    
+    old_buckets = @buckets
+
+    @capacity *= 2
+    @threshold = (@capacity * @load_factor).floor
+    @buckets = Array.new(@capacity) { [] }
+    @size = 0
+
+    old_buckets.each do |bucket|
+      bucket.each { |key, value| set(key, value) }
+    end
+  end
+
+  def get(key)
+    index = bucket_index(key)
+    bucket = @buckets[index]
+
+    bucket.each do |pair|
+      return pair[1] if key[0] == key
+    end
+    nil
+  end
+
+  def has?(key)
+    index = bucket_index(key)
+    bucket = @buckets[index]
+
+    bucket.each do |pair|
+      return true if pair[0] == key
+    end
+    false
+    #or even better to use .any? predicate enumerable,
+    # since false is returned automatically
+    # @buckets[index].any? { |pair| pair[0] == key }
+  end
+
+  def remove(key)
+    index = bucket_index(key)
+    bucket = @buckets[index]
+    removed = []
+
+    bucket.each do |pair|
+      return 
+    end
   end
 end
