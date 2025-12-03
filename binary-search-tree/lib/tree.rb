6460198bebd4aee483706e4cbae0e40708946440
Zarifa <mamadalievazara@gmail.com>
2025-12-04 00:21:36 +0100

    Write a successor method to identify the smallest node from the right

--- binary-search-tree/lib/tree.rb
+++ binary-search-tree/lib/tree.rb
@@ -1,22 +1,46 @@
 class Tree
-  @array_to_sort = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
   attr_reader :root
 
   def initialize
-    @root = buid_tree
+    @root = buid_tree(array)
+    array = array.sort.uniq
   end
 
-  def build_tree(array, arr_start, arr_end)
-    return nil unless arr_start > arr_end
+  def build_tree(array)
+    return nil if array.empty?
+
       mid = arr.length / 2
-      root = Node.new(arr[mid])
+      @root = Node.new(arr[mid])
 
-      root.left = build_tree(array, arr_start, mid - 1)
-      root.right = build_tree(array, arr_start, mid + 1)
-      return root
-
-      sorted = array_to_sort.sort.uniq
-
+      @root.left = build_tree(array[0...mid])
+      @root.right = build_tree(array[mid + 1..-1])
+      @root
   end
-    
+
+  def insert(value)
+    return Node.new if @root.empty?
+
+    if value < @root.data
+      @root.left = insert(@root.left, value)
+    else
+      @root.right = insert(@root.right, value)
+    end
+    @root
+  end
+
+  def getSuccessor(current)
+    current = current.right
+    return nil if current.nil?
+
+    while current.left
+      current = current.left
+    end
+    current
+  end
+  
+  def delete(value)
+    node = @root
+    return node if node.nil?
+    #delete if there is o
+  end
 end
