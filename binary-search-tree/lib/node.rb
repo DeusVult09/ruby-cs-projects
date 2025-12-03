1c18cfc3e6b4bd4e38f5955916139ced5d26f078
Zarifa <mamadalievazara@gmail.com>
2025-12-03 09:21:27 +0100

    Add: Comparable module to compare left vs right nodes later

--- binary-search-tree/lib/node.rb
+++ binary-search-tree/lib/node.rb
@@ -1,7 +1,8 @@
 class Node
+  include Comparable
   attr_accessor :data, :left, :right
-
-  def initialize(value, left, right)
+  
+  def initialize(value, left = nil, right = nil)
     @data = value
     @left = left
     @right = right
@@ -9,5 +10,10 @@ class Node
 
   def children
     [@left, @right]
+  end
+
+  def <=> (other)
+    return nil unless other.is_a?(Node)
+      data <=> other.data
   end
 end
