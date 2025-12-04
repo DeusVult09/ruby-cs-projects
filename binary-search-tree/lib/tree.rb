1aeb354fa906514c4e7705a759aa7f178a9a5ac7
Zarifa <mamadalievazara@gmail.com>
2025-12-04 09:29:12 +0100

    Fix: get_successor method using recursion, typos, missing variable e.g node & root.

--- binary-search-tree/lib/tree.rb
+++ binary-search-tree/lib/tree.rb
@@ -28,19 +28,34 @@ class Tree
     @root
   end
 
-  def getSuccessor(current)
-    current = current.right
-    return nil if current.nil?
+  def get_successor(node)
+    node = node.right
+    return nil if node.nil?
 
-    while current.left
-      current = current.left
+    while node.left
+      node = node.left
     end
-    current
+    node
   end
   
-  def delete(value)
-    node = @root
+  def delete(node, value)
     return node if node.nil?
-    #delete if there is o
+
+    if value > node.data 
+      node.right = delete(node.right, value)
+    elsif value < node.data
+        node.left = delete(node.left, value)
+    end
+    
+
+    #delete if there is 1 child 
+    return node.right if node.left.nil?
+    return node.left if node.right.nil?
+
+    #delete if there are 2 children in the node
+    succ = get_successor(node)
+    node.data = succ.data
+    node.right = delete(node.right, succ.data)
+    node
   end
 end
