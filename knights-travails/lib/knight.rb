f2b8a11f1f8c822621ef69e8bd842685e6868220
Zarifa <mamadalievazara@gmail.com>
2025-12-14 02:12:01 +0100

    Finish the project: write logic to move knight to target using shortedt path

--- knights-travails/lib/knight.rb
+++ knights-travails/lib/knight.rb
@@ -41,24 +41,43 @@ class Knight
     @position = position
   end
 
-  def legal_moves(square)
+  def legal_moves(curr_square)
     x = square[0] #defining a horizontal axis in a square
     y = square[1] #defining a vertical axis
 
-    legal_moves = []
+    moves = [] #creating an array of legal moves
 
-    VALID_MOVES.each do | px, py |
-      new_x = x + px #we are applying valid moves 
+    VALID_MOVES.each do |px, py| #every valid move is visited. 
+      new_x = x + px #we are applying valid moves and calculating candidate square for legal moves
       new_y = y + py #storing in new variables
 
-      if new_x.between?(0..7) && new_y.between?(0..7)
-        legal_moves << [new_x, new_y] #only if moves are within board edges
+      if new_x.between?(0..7) && new_y.between?(0..7) 
+        moves << [new_x, new_y] #only if moves are within board edges add them to a legal moves list
       end
     end
-    legal_moves #return them
+    moves #return them
   end
 
-  def knight_moves(start_pos, target_pos)
-    
+  def knight_moves(start_pos, target_pos) 
+    queue = [[start_pos]]
+    visited_paths = []
+
+    while !queue.empty?
+      current_path = queue.shift
+      curr_square = current_path.last
+      possible_moves = legal_moves(curr_square)
+
+      if curr_square == target_pos
+        puts "You made it in #{current_path.length - 1}! Here is your path: #{current_path}"
+        return current_path
+      else
+        possible_moves.each do |position|
+          next if visited_paths.include?(position)
+          visited_paths << position
+          new_path = current_path + [position]
+          queue << new_path
+        end
+      end
+    end
   end
 end
